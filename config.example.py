ada2875ed42949220b6d5ac462b43a01689d90e8
CNC Monitor <cnc-monitor@example.local>
2026-05-03 17:20:34 +0300

    add config example template (no secrets)

---
+++ config.example.py
@@ -0,0 +1,23 @@
+﻿# Example configuration file
+# This file is safe to commit to Git (no real secrets)
+
+TOKEN = "PUT_YOUR_TOKEN_HERE"
+CHAT_ID = "PUT_YOUR_CHAT_ID_HERE"
+
+MACHINES = [
+    {
+        "NAME": "NC STUDIO",
+        "TYPE": "NCSTUDIO",
+        "WATCH_FILE": r"Сюди вставляємо шлях до файлу\NCSTUDIO.DYN",
+        "LOG_FILE": r"Сюди вставляємо шлях до файлу\NCSTUDIO.LOG",
+        "IDLE_TIMEOUT": 15,
+        "PAUSE_TIMEOUT": 15
+    },
+    {
+        "NAME": "ENGRAVE 80E",
+        "TYPE": "ENGRAVE",
+        "WATCH_FILE": r"Сюди вставляємо шлях до файлу\ws080e1.grv",
+        "IDLE_TIMEOUT": 20,
+        "PAUSE_TIMEOUT": 10
+    }
+]
